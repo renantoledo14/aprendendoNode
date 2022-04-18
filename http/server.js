625d7157edd8932d8be4f79e536ca4c5155f1a4f
Renan de Oliveira Toledo <59629350+renantoledo14@users.noreply.github.com>
2022-04-18 14:00:34 -0300

    add http

--- http/server.js
+++ http/server.js
@@ -4,19 +4,22 @@ const path = require('path')
 
 http.createServer((req, res) => {
 
-	if (req.url === '/'){
-		fs.readFile(
-			path.join(__dirname, 'public', 'index.html'),
-			(err, content) => {
-				if(err) throw err
+	const file = req.url === '/' ? 'index.html' : req.url
+	const filePath = path.join(__dirname, 'public', file)
+	const extname = path.extname(filePath)
 
-				res.end(content)
-			}
-		)
-	}
-	if (req.url === '/contato')
-		res.end('<h1>Contato</h1>')
-	
+	const allowedFileTypes = ['.html', '.css', '.js']
+	const allowed = allowedFileTypes.find(item => item == extname)
 
+	if(!allowed) return
+
+	fs.readFile(
+		filePath,
+		(err, content) => {
+			if(err) throw err
+
+			res.end(content)
+		}
+	)
 
 }).listen(5000, () => console.log('server is running'))
